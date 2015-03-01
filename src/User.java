c7bd051378ea68bdfd09f1ae12b89b1dd4b2e4c1
SivaLalita <lalita.vissamsetty@gmail.com>
2015-02-28 19:37:34 -0800

    InitialCommit

---
+++ src/User.java
@@ -0,0 +1,14 @@
+
+public class User {
+    private int id;
+	private String firstName;
+	private String lastName;
+	private int cardNumber;
+	
+	User(int id, String firstName,String lastName, int cardNumber){
+		this.id=id;
+		this.firstName=firstName;
+		this.lastName=lastName;
+		this.cardNumber=cardNumber;
+	}
+}
