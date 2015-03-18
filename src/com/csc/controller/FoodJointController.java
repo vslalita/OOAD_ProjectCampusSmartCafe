e0ee018ba446ecec1ab26540a33f9859fbfcd1ca
Visamsetty, Deva <Deva.Visamsetty@disney.com>
2015-03-17 21:40:37 -0700

    Remove user as parameter from FoodJointController -> updateFoodPreference

--- src/com/csc/controller/FoodJointController.java
+++ src/com/csc/controller/FoodJointController.java
@@ -2,11 +2,16 @@ package com.csc.controller;
 
 import com.csc.CurrentSession;
 import com.csc.model.FoodJoint;
+import com.csc.model.FoodPurchaseTransaction;
+import com.csc.service.FoodJointService;
 
 public class FoodJointController {
 	
 	public void selectFoodJoint(FoodJoint foodJoint){
 		CurrentSession.getInstance().setCurrentFoodJoint(foodJoint);
 	}
-
+	public FoodPurchaseTransaction getFoodPurchaseTransactionDetails(int orderTransactionId){
+		FoodJointService foodJointService = CSCServiceContext.getFoodJointService();
+		return foodJointService.getFoodPurchaseTransactionDetails(orderTransactionId);	
+	}
 }
